96140cb40cc9232e40a14f3301ced50ded9ac024
alpine57 <kamutileeon@gmail.com>
2023-07-26 13:21:34 -0700

    bum

--- envbuiltin.c
+++ envbuiltin.c
@@ -70,9 +70,9 @@ char *m_getenv(const char *ident)
 	for (int x = 0; environ[x]; x++)
 
 	{
-	if (strncmp(environ[x], ident, row) == 0 && environ[x][row] == '=' && environ[x][row + 1] != '\0')
+	if (strncmp(environ[x], ident, row) == 0 && environ[x][row] == '=' )
 
-	return (&environ[x] + [row + 1]);
+	return (&environ[x] [row + 1]);
 	}
 
 	return (NULL);
